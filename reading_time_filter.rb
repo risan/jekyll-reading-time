aec063258bcb0f39eb9e08a872f73cc924efaec3
berkant <berkantkorkmaz@outlook.com>
2023-11-21 16:06:05 +0300

    a complete logical approach

--- reading_time_filter.rb
+++ reading_time_filter.rb
@@ -3,7 +3,7 @@ module ReadingTimeFilter
 
   def reading_time(input)
     # Get words count.
-    total_words = get_plain_text(input).split.size
+    total_words = get_plain_text(input).split.length
 
     # Load configuration.
     config = @context.registers[:site].config["reading_time"]
@@ -23,23 +23,9 @@ module ReadingTimeFilter
     words_per_minute = 180
 
     # Calculate reading time.
-    case total_words
-    when 0 .. 89
-      return "30 #{second_plural}"
-    when 90 .. 269
-      return "1 #{minute_singular}"
-    when 270 .. 449
-      return "2 #{minute_plural}"
-    when 450 .. 629
-      return "3 #{minute_plural}"
-    when 630 .. 809
-      return "4 #{minute_plural}"
-    when 810 .. 990
-      return "5 #{minute_plural}"
-    else
-      minutes = ( total_words / words_per_minute ).floor
-      return "#{minutes} #{minute_plural}";
-    end
+    
+    minutes = ((total_words / words_per_minute)+(0.9)).round
+    return "#{minutes} #{minute_plural}";
   end
 
   def get_plain_text(input)
